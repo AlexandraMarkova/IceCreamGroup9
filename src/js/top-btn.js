7dcadc99bba06e67907e2765c2bd369222c15eac
Olga Konopleva <olenenok.she@gmail.com>
2020-12-25 22:08:07 +0200

    кнопка вверх

--- src/js/top-btn.js
+++ src/js/top-btn.js
@@ -1,16 +1,16 @@
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
+const btnRef = document.querySelector('#myBtn');
 function scrollFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        document.getElementById("myBtn").style.display = "block";
+        btnRef.style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        btnRef.style.display = "none";
     }
-}
-
+};
+btnRef.addEventListener('click', topFunction);
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+};
